de833d2708e460deaddf54002b437a38a5790c77
槿南11 <3152598423@qq.com>
2025-01-19 19:40:13 +0800

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -15,7 +15,7 @@
         </span>
         <!-- 以下信息请不要修改哦 -->
         <span class="hidden">
-          &amp;&nbsp;
+          &amp;
           <a :href="config.github" target="_blank">
             {{ config.author }}
           </a>
